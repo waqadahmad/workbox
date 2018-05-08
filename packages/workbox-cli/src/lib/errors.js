b1c050c53127a6ad70ff29e5f8dfc334adc3cc8a
waqadahmad <39062870+waqadahmad@users.noreply.github.com>
2018-05-08 13:40:28 +0500

    Update errors.js

--- packages/workbox-cli/src/lib/errors.js
+++ packages/workbox-cli/src/lib/errors.js
@@ -1,4 +1,4 @@
-/**
+/html**
  * Copyright 2017 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -37,3 +37,4 @@ module.exports = {
     file. Please try again with a file containing
     'workbox.precaching.precacheAndRoute([])'.`,
 };
+<Html/Body>
